15d948c32ab4a5a099fd6a15aaa9496107573f75
RZFury <48477044+rzfury@users.noreply.github.com>
2023-04-06 16:21:20 +0700

    Init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <div class="flex items-center justify-center p-4 bg-black text-white">
+      <span class="line-through">REACT</span>&nbsp;<span class="font-bold">SOLIDJS</span>
+    </div>
+  );
+}
+
+export default App;
