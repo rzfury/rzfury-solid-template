8c936b6c76737c26e51d2ca58ba8565b500212bb
RZFury <48477044+rzfury@users.noreply.github.com>
2023-04-06 16:24:48 +0700

    fix: fixes

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { render } from 'solid-js/web';
 import App from './App';
 import './index.css';
 
-const root = document.getElementById('root');
+const root = document.getElementById('app');
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
